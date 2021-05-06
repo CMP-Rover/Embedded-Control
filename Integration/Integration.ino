2565d83fc0bcc1ee4572bdae8a45f8af0544550f
PeterAyad <peter.ayad00@eng-st.cu.edu.eg>
2021-05-06 14:38:03 +0200

    Integration Started

---
+++ Integration/Integration.ino
@@ -0,0 +1,58 @@
+#include"BMP180.h"
+#include "GPS.h"
+#include "PID_Control.h"
+#include "MQ7.h"
+#define ENCODER_A 2
+#define ENCODER_B 3
+#define PWM 5
+#define IN2 6
+#define IN1 7
+_BMP180_ bmpp;
+//GPS G;
+MQ7 Sensor(0, 2, 1000);
+void setup()
+{
+  Serial.begin(9600);
+  bmpp.begin();
+  Sensor.Warmup();
+  pinMode(ENCODER_A, INPUT); // ENCODER_A as Input
+  pinMode(ENCODER_B, INPUT); // ENCODER_B as Input
+  pinMode(IN2, OUTPUT); // MOTOR_CW as Output
+  pinMode(IN1, OUTPUT); // MOTOR_CW as Output
+  pinMode(PWM, OUTPUT); // MOTOR_CW as Output
+  setPIDTarget(400);
+  setPIDParameters (24, 0, 16);
+  setMotorEncoders(ENCODER_A, ENCODER_B);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_A), readEncoder, RISING);
+}
+
+void loop()
+{
+  int i =0 ;
+  float pos = getCurrentPosition();
+  float conSignal = computePID(pos);
+  convertSignal(conSignal , PWM ,  IN1 , IN2);
+  if (i % 2000 == 0)
+  {
+    float temp = 0, pressure = 0, altitude = 0;
+    bmpp.getTemperature(&temp);
+    bmpp.getPressure(&pressure);
+    altitude = bmpp.pressureToAltitude(101325, pressure);
+    int ppm = Sensor.GetGasConcentration();
+    bool gasDetected = Sensor.IsGasDetected();
+//    Sensor.PrintMQ7Data();
+    Serial.print("Temperature:");
+    Serial.println(temp);
+    Serial.print("Pressure:");
+    Serial.println(pressure / 100.0);
+    Serial.print("Altitude:");
+    Serial.println(altitude);
+    Serial.print("Pos : ");
+    Serial.println(pos);
+  }
+  i++;
+  //  while (!G.readPer())
+  //  {
+  //  }
+  //  G.printAll();
+}
---
+++ Integration/MQ7.h
@@ -0,0 +1,22 @@
+#define MIN_PPM 10
+#define MAX_PPM 500
+#define WARMUP_PERIOD 1000 //meliseconds
+#define ANALOG_PIN 0
+#define DIGITAL_PIN 2
+#define MAX_OUTPUT 4.3
+#define MIN_OUTPUT 2.5
+
+class MQ7
+{
+private:
+    int analog_pin;
+    int digital_pin;
+    int warmup_period; 
+public:
+    MQ7(int analog, int digital, int warmup);
+    int GetGasConcentration();
+    bool IsGasDetected();
+    void PrintMQ7Data();
+    void Warmup();
+    ~MQ7();
+};
