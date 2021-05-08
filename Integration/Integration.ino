dc6c2ad9bed19661bad66d08bb4056e2cf3bcafb
PeterAyad <peter.ayad00@eng-st.cu.edu.eg>
2021-05-08 11:11:46 +0200

    SD Card Added

--- Integration/Integration.ino
+++ Integration/Integration.ino
@@ -4,16 +4,22 @@
 #include "MQ7.h"
 #include "MQ2.h"
 #include "DHT.h"
+#include <SPI.h>
+#include <SD.h>
+
 #define ENCODER_A 2
 #define ENCODER_B 3
 #define PWM 5
 #define IN2 6
 #define IN1 7
+
 _BMP180_ bmpp;
 GPS G;
 MQ7 Sensor(0, 8, 1000);
 MQ2 gasSensor(0, 9, 2000);
-DHT D1(10);
+DHT D1(4);
+File file;
+
 void setup()
 {
   Serial.begin(9600);
@@ -28,8 +34,16 @@ void setup()
   setPIDParameters (24, 0, 16);
   setMotorEncoders(ENCODER_A, ENCODER_B);
   attachInterrupt(digitalPinToInterrupt(ENCODER_A), readEncoder, RISING);
-  //Serial.println("Gas sensor warming up!");
   gasSensor.Warmup();
+  if (SD.begin(10))
+  {
+    if(SD.exists("data.txt"))
+    {
+      SD.remove("data.txt");
+    }
+    Serial.println("Card initialized!");
+  }
+  digitalWrite(10, HIGH);
 }
 
 void loop()
@@ -42,20 +56,20 @@ void loop()
   if (i % 5000 == 0)
   {
     float temp = 0, pressure = 0, altitude = 0;
-    int MQ2Data=0 , MQ7Data=0, DHTData=0;
-    bool MQ2Detected=false ,MQ7Detected=false;
+    int MQ2Data = 0 , MQ7Data = 0, DHTData = 0;
+    bool MQ2Detected = false , MQ7Detected = false;
     unsigned char RH_int, T_int, RH_dec, T_dec;
     bmpp.getTemperature(&temp);
     bmpp.getPressure(&pressure);
     altitude = bmpp.pressureToAltitude(101325, pressure);
     //MQ7
-    Sensor.PrintMQ7Data(&MQ7Data,&MQ7Detected);
+    Sensor.PrintMQ7Data(&MQ7Data, &MQ7Detected);
     //MQ2
-    gasSensor.PrintMQ2Data(&MQ2Data,&MQ2Detected);
+    gasSensor.PrintMQ2Data(&MQ2Data, &MQ2Detected);
     //DHT
     D1.GetData();
-    D1.printT(&T_int,&T_dec);
-    D1.printRH(&RH_int,&RH_dec);
+    D1.printT(&T_int, &T_dec);
+    D1.printRH(&RH_int, &RH_dec);
     if ((x - 11000) > 0)
     {
       G.readData(150);
@@ -70,8 +84,28 @@ void loop()
     Serial.println(altitude);
     Serial.print("Pos : ");
     Serial.println(pos);
+
+    file = SD.open("data.txt", FILE_WRITE);
+    if (!file)
+    {
+      Serial.println("File not detected!");
+    }
+    else
+    {
+      Serial.println("Printing...");
+      file.print("Temperature:");
+      file.println(String(temp));
+      file.print("Pressure:");
+      file.println(String(pressure / 100.0));
+      file.print("Altitude:");
+      file.println(String(altitude));
+      file.print("Pos : ");
+      file.println(String(pos));
+      file.println("--------------------------------------------");
+      file.close();
+    }
+    Serial.println("End!");
+    Serial.println("--------------------------------------------");
   }
   i++;
-  
-  
 }
