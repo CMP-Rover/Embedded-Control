0a86c603fb31bc65f760f3cae2e7df90798f4999
mohamed99akram <mohamed1999akram@gmail.com>
2021-05-18 15:25:59 +0200

    using SoftwareSerial with GPS

--- GPS/Code/GPS2.ino
+++ GPS/Code/GPS2.ino
@@ -1,15 +1,18 @@
 #include "GPS.h"
-GPS G;
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(2, 3);
+GPS G(&mySerial);
 void setup()
 {
     Serial.begin(9600);
+    mySerial.begin(9600);
 }
 
 void loop()
 {
-
-    if (G.readPer())
+    if (millis() % 10000 == 0)
     {
-        G.printAll();//the same as://  Serial.println(G.getPosition());
+        G.readData();
+        G.printAll();
     }
 }
